dee38eabb6070f5f7992088ed2061328616755c5
Julian Zulfikar <jzulfika@uci.edu>
2023-09-29 15:58:33 -0700

    Introduction component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import selfie from "./images/IMG_0186.jpg";
+import Introduction from './components/Introduction/Introduction';
 
 function App() {
   return (
     <div className="App">
-        <h1>Julian Zulfikar - Under Construction</h1>
-        <p>
-            Hello! If you are reading this, the website is currently under construction.
-            <br></br><br></br>
-            To see my current portfolio (not styled to modern standards), click <a href="https://www.ics.uci.edu/~jzulfika">here</a>!
-            <br></br><br></br>
-            Here is a picture of me at a creek in the beautiful East Bay Area.
-        </p>
-        <img src={selfie} alt="Picture of Julian" height="400px"/>
+        <Introduction title="Introduction"></Introduction>
     </div>
   );
 }
---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,30 @@
+import './Introduction.css';
+import pic from './IMG_0186.jpg';
+import github from './github.png';
+import linkedin from './linkedin.png';
+import mail from './mail.png';
+
+function Introduction(props) {
+    return (
+        <div className="container">
+            <div className="Header">
+                {props.title}
+            </div>
+            <div className="Introduction">
+                <img src={pic} id='pic' alt="Picture of Julian Zulfikar"></img>
+                <div className="allText">
+                    <div className="nameText">Hello! I am</div>
+                    <div className="fullName">Julian Zulfikar</div>
+                    <div className="roleText">Computer Science @ UCI</div>
+                </div>
+                <div className="Socials">
+                    <a href="https://github.com/julian-z" target="_blank"><img src={github} className="social" alt="GitHub"></img></a>
+                    <a href="https://www.linkedin.com/in/julian-z/" target="_blank"><img src={linkedin} className="social" alt="LinkedIn"></img></a>
+                    <a href="mailto:jzulfika@uci.edu" target="_blank"><img src={mail} className="social" alt="Email"></img></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
