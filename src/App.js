773f19601ed59a805e1bb9f97b6c146b3683f121
Julian Zulfikar <julianzulfikar@gmail.com>
2024-02-21 14:48:33 -0800

    Updated for the most part

--- src/App.js
+++ src/App.js
@@ -1,234 +1,276 @@
-import './App.css';
-import './NavigationBar.css';
-import React, {useState, useEffect} from 'react';
+import "./App.css";
+import "./NavigationBar.css";
+import React, { useState, useEffect } from "react";
 
-import Introduction from './components/Introduction/Introduction';
-import About from './components/About/About';
-import Project from './components/Project/Project';
-import Experience from './components/Experience/Experience';
+import Introduction from "./components/Introduction/Introduction";
+import About from "./components/About/About";
+import Project from "./components/Project/Project";
+import Experience from "./components/Experience/Experience";
 
-import Next from './next.png'
-import Prev from './prev.png'
-import logo from './logo-black.png';
-import dropdown from './dropdown.png';
+import Next from "./next.png";
+import Prev from "./prev.png";
+import logo from "./logo-black.png";
+import dropdown from "./dropdown.png";
 
 function App() {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
-    document.title = 'Julian Zulfikar';
+    document.title = "Julian Zulfikar";
   }, []);
 
   const handleClickNext = () => {
-    setCurrent(current+1);
-  }
+    setCurrent(current + 1);
+  };
 
   const handleClickPrev = () => {
-    setCurrent(current-1);
-  }
+    setCurrent(current - 1);
+  };
 
   const createNavBar = (currentPage, opacity) => {
     const pages = [
       <button onClick={() => setCurrent(0)}>Introduction</button>,
       <button onClick={() => setCurrent(1)}>About</button>,
       <button onClick={() => setCurrent(2)}>Projects</button>,
-      <button onClick={() => setCurrent(5)}>Experience</button>
+      <button onClick={() => setCurrent(5)}>Experience</button>,
     ];
-    
+
     if (currentPage === 0) {
-        pages[0] = <button onClick={() => setCurrent(0)} style={{opacity: opacity}}>Introduction</button>;
+      pages[0] = (
+        <button onClick={() => setCurrent(0)} style={{ opacity: opacity }}>
+          Introduction
+        </button>
+      );
+    } else if (currentPage === 1) {
+      pages[1] = (
+        <button onClick={() => setCurrent(1)} style={{ opacity: opacity }}>
+          About
+        </button>
+      );
+    } else if (currentPage === 5) {
+      pages[3] = (
+        <button onClick={() => setCurrent(5)} style={{ opacity: opacity }}>
+          Experience
+        </button>
+      );
+    } else {
+      pages[2] = (
+        <button onClick={() => setCurrent(2)} style={{ opacity: opacity }}>
+          Projects
+        </button>
+      );
     }
-    else if (currentPage === 1) {
-        pages[1] = <button onClick={() => setCurrent(1)} style={{opacity: opacity}}>About</button>;
-    }
-    else if (currentPage === 5) {
-        pages[3] = <button onClick={() => setCurrent(5)} style={{opacity: opacity}}>Experience</button>;
-    }
-    else {
-        pages[2] = <button onClick={() => setCurrent(2)} style={{opacity: opacity}}>Projects</button>;
-    }
-    
+
     return pages;
-  }
+  };
 
   if (current === 0) {
     return (
       <div className="App">
-          <Introduction title="Introduction"></Introduction>
-          <button onClick={handleClickNext}><img src={Next} alt="Next" className="next" id="introButton"></img></button>
+        <Introduction title="Introduction"></Introduction>
+        <button onClick={handleClickNext}>
+          <img src={Next} alt="Next" className="next" id="introButton"></img>
+        </button>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
 
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
+
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
+          </div>
+        </div>
       </div>
     );
   }
   if (current === 1) {
     return (
       <div className="App">
-          <About title="About Me"></About>
-          <div className="Navigation">
-            <button onClick={handleClickPrev}><img src={Prev} alt="Previous" className="prev"></img></button>
-            <button onClick={handleClickNext}><img src={Next} alt="Next" className="next"></img></button>
+        <About title="About Me"></About>
+        <div className="Navigation">
+          <button onClick={handleClickPrev}>
+            <img src={Prev} alt="Previous" className="prev"></img>
+          </button>
+          <button onClick={handleClickNext}>
+            <img src={Next} alt="Next" className="next"></img>
+          </button>
+        </div>
+
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
+
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
-
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
           </div>
+        </div>
       </div>
     );
   }
   if (current === 2) {
     return (
       <div className="App">
-          <Project title="(1/3) QuickYelp"
-            name="quickyelp"
-            github="https://github.com/julian-z/quickyelp"
-            website="https://www.quickyelp.com"
-            color={[191, 58, 34]}
-            desc="A dynamic AI chatbot which scans the reviews and information of any business on Yelp."
-            bullets={["Incorporated the LangChain framework to create indexes of data from the Yelp Fusion API, streamlining the training process of OpenAI’s GPT-4.", 
-                    "Integrated in-memory storage, Redis, for rate limiting, index retrieval, and automatic data expiration.",
-                    "Built a functional and responsive user interface using HTML, CSS, JavaScript, and the React framework.",
-                    "Successfully handling a growing number of 150+ chat sessions."]}
-            stack={['python', 'html', 'css', 'javascript', 'react']}>
-          </Project>
-          <div className="Navigation">
-            <button onClick={handleClickPrev}><img src={Prev} alt="Previous" className="prev"></img></button>
-            <button onClick={handleClickNext}><img src={Next} alt="Next" className="next"></img></button>
+        <Project
+          title="(1/3) QuickYelp"
+          name="quickyelp"
+          github="https://github.com/julian-z/quickyelp"
+          website="https://www.quickyelp.com"
+          color={[0, 0, 0]}
+          desc="A dynamic AI chatbot which scans the reviews and information of any business on Yelp."
+          bullets={[
+            "Incorporated the LangChain framework to create 2 FAISS indexes of data from the Yelp Fusion API, streamlining the training process of OpenAI’s GPT-4 model.",
+            "Integrated in-memory storage, Redis, for rate limiting, index retrieval, and automatic data expiration.",
+            "Built a functional and responsive user interface using HTML, CSS, JavaScript, and the React framework.",
+            "Successfully handling a growing number of 250+ chat sessions in a production environment.",
+          ]}
+          stack={["python", "react", "html", "css", "javascript"]}
+        ></Project>
+        <div className="Navigation">
+          <button onClick={handleClickPrev}>
+            <img src={Prev} alt="Previous" className="prev"></img>
+          </button>
+          <button onClick={handleClickNext}>
+            <img src={Next} alt="Next" className="next"></img>
+          </button>
+        </div>
+
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
+
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
-
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
           </div>
+        </div>
       </div>
     );
   }
   if (current === 3) {
     return (
       <div className="App">
-          <Project title="(2/3) ZotPlanner"
-            name="zotplanner"
-            github="https://github.com/julian-z/zotplanner"
-            website="https://zotplanner.pythonanywhere.com/"
-            color={[22, 173, 97]}
-            desc="A UCI course search engine and graph generator which warns users about prerequisites."
-            bullets={["Implemented a web-crawler to extract and categorize data from 6,000+ courses.",
-                    "Enabled the construction of an inverted index, drastically reducing the runtime by 99% due to instantaneous retrieval operations.",
-                    "Leveraged the tokenization abilities of the Natural Language Toolkit (NLTK) in tandem with TF-IDF scoring to accurately rank queries."]}
-            stack={['python', 'html', 'css']}>
-          </Project>
-          <div className="Navigation">
-            <button onClick={handleClickPrev}><img src={Prev} alt="Previous" className="prev"></img></button>
-            <button onClick={handleClickNext}><img src={Next} alt="Next" className="next"></img></button>
+        <Project
+          title="(2/3) ZotServices"
+          name="zotplanner"
+          github="https://github.com/julian-z/zotservices"
+          website="https://github.com/julian-z/zotservices"
+          color={[0, 0, 0]}
+          desc="A UCI student-based marketplace for side-hustles and services."
+          bullets={[
+            "Created a Django REST API for user and service information, designed a dynamic 5-page React interface populated by the API data.",
+            "Designed a 3-table schema for a SQLite relational database maintaining categorical attributes, resulting in a filtering system with a response time of less than 90 ms.",
+            "Collaborated within a 4-member team for UCI's WebJam 2023, presented the application to a panel of 3 professors and an audience of 110+ students.",
+          ]}
+          stack={["python", "react", "html", "css", "javascript"]}
+        ></Project>
+        <div className="Navigation">
+          <button onClick={handleClickPrev}>
+            <img src={Prev} alt="Previous" className="prev"></img>
+          </button>
+          <button onClick={handleClickNext}>
+            <img src={Next} alt="Next" className="next"></img>
+          </button>
+        </div>
+
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
+
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
-
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
           </div>
+        </div>
       </div>
     );
   }
   if (current === 4) {
     return (
       <div className="App">
-          <Project title="(3/3) Minesweeper Lab"
-            name="minesweeperlab"
-            github="https://github.com/julian-z/minesweeper-lab"
-            website="https://www.youtube.com/watch?v=Fi9VODiHQNg"
-            color={[89, 108, 196]}
-            desc="A spin-off Minesweeper game designed to train the puzzle-solving abilities of players."
-            bullets={["Designed a schema for the creation of a relational database using the SQLite API, providing a means of progression tracking and a competitive environment for local players.",
-                    "Implemented an auto-solving AI as a tutorial demonstration which uses pattern recognition, algorithms, and logical rules of the base Minesweeper game.",
-                    "Built a fully functional Graphical User Interface (GUI) with the SDL2 library."]}
-            stack={['cplusplus', 'sql']}>
-          </Project>
-          <div className="Navigation">
-            <button onClick={handleClickPrev}><img src={Prev} alt="Previous" className="prev"></img></button>
-            <button onClick={handleClickNext}><img src={Next} alt="Next" className="next"></img></button>
+        <Project
+          title="(3/3) Minesweeper Lab"
+          name="minesweeperlab"
+          github="https://github.com/julian-z/minesweeper-lab"
+          website="https://www.youtube.com/watch?v=Fi9VODiHQNg"
+          color={[0, 0, 0]}
+          desc="A spin-off Minesweeper game designed to train the puzzle-solving abilities of players."
+          bullets={[
+            "Designed a schema for the creation of a relational database using the SQLite API, providing a means of progression tracking and a competitive environment for local players.",
+            "Implemented an auto-solving AI as a tutorial demonstration which uses pattern recognition, algorithms, and logical rules of the base Minesweeper game.",
+            "Built a fully functional Graphical User Interface (GUI) with the SDL2 library.",
+          ]}
+          stack={["cplusplus", "sql"]}
+        ></Project>
+        <div className="Navigation">
+          <button onClick={handleClickPrev}>
+            <img src={Prev} alt="Previous" className="prev"></img>
+          </button>
+          <button onClick={handleClickNext}>
+            <img src={Next} alt="Next" className="next"></img>
+          </button>
+        </div>
+
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
+
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
-
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
           </div>
+        </div>
       </div>
     );
   }
   if (current === 5) {
     return (
       <div className="App">
-          <Experience></Experience>
-          <button onClick={handleClickPrev}><img src={Prev} alt="Previous" className="prev"></img></button>
+        <Experience></Experience>
+        <button onClick={handleClickPrev}>
+          <img src={Prev} alt="Previous" className="prev"></img>
+        </button>
 
-          <div className="NavigationBar">
-            <img src={logo} alt="JZ" id="JZ"></img>
+        <div className="NavigationBar">
+          <img src={logo} alt="JZ" id="JZ"></img>
 
-            <div className="NavigationOptions">
-                {createNavBar(current, '0.5')}
-            </div>
-
-            <div className="NavigationDropdown">
-              <button><img src={dropdown} alt="Menu"></img></button>
-              <div className="DropdownContent">
-                {createNavBar(current, '1')}
-              </div>
-            </div>
+          <div className="NavigationOptions">
+            {createNavBar(current, "0.5")}
           </div>
+
+          <div className="NavigationDropdown">
+            <button>
+              <img src={dropdown} alt="Menu"></img>
+            </button>
+            <div className="DropdownContent">{createNavBar(current, "1")}</div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,53 +1,90 @@
-import './Experience.css';
-import UCI from './uci.png';
-import ZotMe from './ZotMe.png';
+import "./Experience.css";
+import UCI from "./uci.png";
+import ZotMe from "./ZotMe.png";
+import GBCS from "./gbcs.png";
+import Cisco from "./cisco.png";
 
 function Experience() {
-    return (
-        <div className="container">
-            <div className="Header experience">
-                Experience & Activities
+  return (
+    <div className="container">
+      <div className="Header experience">Work Experience</div>
+      <div className="Experience">
+        <div className="section">
+          <img src={Cisco} alt="Cisco" className="experiencePic"></img>
+          <div>
+            <div className="experienceTitle">Software Engineer Intern</div>
+            <div className="experienceCompany">Cisco</div>
+            <div className="experienceBullets">
+              <ul id="experienceUL">
+                <li>
+                  Incoming Software Engineer Intern for Summer 2024, Mass
+                  Infrastructure Group (MIG)
+                </li>
+              </ul>
             </div>
-            <div className="Experience">
-                <div className="section">
-                    <img src={UCI} alt="UCI" className="experiencePic"></img>
-                    <div>
-                        <div className="experienceTitle">
-                            Python Learning Assistant
-                        </div>
-                        <div className="experienceCompany">
-                            University of California, Irvine
-                        </div>
-                        <div className="experienceBullets">
-                            <ul id="experienceUL">
-                                <li>Supported 430+ students across 2 lecture sections in building a strong foundation of object-oriented programming.</li>
-                                <li>Conducted office hours (4 hours/week) to deliver guidance for students working on Python programming assignments, encouraging the development of effective debugging and problem-solving techniques.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <img src={ZotMe} alt="ZotMe" className="experiencePic"></img>
-                    <div>
-                        <div className="experienceTitle">
-                            User Interface Designer
-                        </div>
-                        <div className="experienceCompany">
-                            UCI Mental Health Hackathon - ZotMe
-                        </div>
-                        <div className="experienceBullets">
-                            <ul id="experienceUL">
-                                <li>Collaborated as a vital member of a 5-person team to conceptualize and pitch ZotMe, a college-based social media application focusing on friend and club-seeking activities to combat mental health issues caused by loneliness.</li>
-                                <li>Engaged in the thorough UI/UX design process, actively crafting 2 intuitive and engaging iOS/Android interface mock-ups.</li>
-                                <li>Presented the application to an audience of over 125 students and 5 judges, ultimately securing a funding boost of $1,000.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    )
+
+        <div className="section">
+          <img src={GBCS} alt="GBCS Group" className="experiencePic"></img>
+          <div>
+            <div className="experienceTitle">Software Developer Intern</div>
+            <div className="experienceCompany">GBCS Group</div>
+            <div className="experienceBullets">
+              <ul id="experienceUL">
+                <li>
+                  Revived a discontinued vendor system project as the sole
+                  full-stack developer within a 5-member Scrum team.
+                </li>
+                <li>
+                  Implemented login authentication, password reset emails, and
+                  profile customization for 240+ users and 28 employees.
+                </li>
+                <li>
+                  Increased codebase modularity 32% by connecting 2 API
+                  interfaces to integrate the vendor system with the company's
+                  current infrastructure, enabled communication between vendors
+                  and clients within 14+ companies.
+                </li>
+                <li>
+                  Utilized Python, Django, MySQL, React, JavaScript, Microsoft
+                  Azure, and Postman.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <img src={UCI} alt="UCI" className="experiencePic"></img>
+          <div>
+            <div className="experienceTitle">
+              Undergraduate Learning Assistant
+            </div>
+            <div className="experienceCompany">
+              University of California, Irvine
+            </div>
+            <div className="experienceBullets">
+              <ul id="experienceUL">
+                <li>
+                  Mentored 220+ students within a 15-member team in building a
+                  strong foundation of object-oriented programming.
+                </li>
+                <li>
+                  Conducted office hours (3 hours/week) to deliver guidance for
+                  debugging Python assignments.
+                </li>
+                <li>
+                  Graded 8 formative assessments for 10-12 students, providing
+                  personalized feedback to gauge comprehension.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
